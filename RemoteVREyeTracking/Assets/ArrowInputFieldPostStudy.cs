5c407427dea83ff17e91ce59d2f38afa7bdbe76a
Ante941 <72032365+Ante941@users.noreply.github.com>
2022-01-28 18:40:11 +0100

    Post Study select input field implemented

--- RemoteVREyeTracking/Assets/ArrowInputFieldPostStudy.cs
+++ RemoteVREyeTracking/Assets/ArrowInputFieldPostStudy.cs
@@ -23,7 +23,17 @@ public class ArrowInputFieldPostStudy : MonoBehaviour
     public GameObject moreInstructionObject;
     public GameObject vrCameraObject;
 
+    public GameObject lastText;
+    public GameObject postStudy;
+    public GameObject keyboard;
+    public GameObject canvasKeyboard;
+
     public int InputSelected;
+
+    public GameObject arrowUpKey;
+    public GameObject arrowDownKey;
+    private Button  buttonArrowUp;
+    private Button  buttonArrowDown;
 
     private string distractionText;
     private string distractionOnScreenText;
@@ -32,7 +42,8 @@ public class ArrowInputFieldPostStudy : MonoBehaviour
     private string moreInstructionText;
     private string vrCameraText;
 
-    public GameObject rightHandLP;
+    public float buttonBlockedDown = 0;
+    public float buttonBlockedUp = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +54,8 @@ public class ArrowInputFieldPostStudy : MonoBehaviour
         answerWrongText = answerWrong.GetComponent<TMP_InputField>().text;
         moreInstructionText = moreInstruction.GetComponent<TMP_InputField>().text;
         vrCameraText = vrCamera.GetComponent<TMP_InputField>().text;
+        buttonArrowUp = arrowUpKey.GetComponent<Button>();
+        buttonArrowDown = arrowDownKey.GetComponent<Button>();
 
 
     }
@@ -50,124 +63,120 @@ public class ArrowInputFieldPostStudy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        buttonBlockedDown += Time.deltaTime;
+        buttonBlockedUp += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Return))
+    }
+
+    public void filledIn()
+    {
+        if (distraction.text.Trim().Length == 0)
         {
-            if (distraction.text.Trim().Length == 0)
-            {
-                distractionObject.SetActive(true);
-            }
+            distractionObject.SetActive(true);
+        }
 
-            if (distractionOnScreen.text.Trim().Length == 0)
-            {
-                distractionOnSObject.SetActive(true);
-            }
+        if (distractionOnScreen.text.Trim().Length == 0)
+        {
+            distractionOnSObject.SetActive(true);
+        }
 
-            if (difficultyStudy.text.Trim().Length == 0)
-            {
-                difficultyStudyObject.SetActive(true);
-            }
+        if (difficultyStudy.text.Trim().Length == 0)
+        {
+            difficultyStudyObject.SetActive(true);
+        }
 
-            if (answerWrong.text.Trim().Length == 0)
-            {
-                answerWrongObject.SetActive(true);
-            }
+        if (answerWrong.text.Trim().Length == 0)
+        {
+            answerWrongObject.SetActive(true);
+        }
 
-            if (moreInstruction.text.Trim().Length == 0)
-            {
-                moreInstructionObject.SetActive(true);
-            }
+        if (moreInstruction.text.Trim().Length == 0)
+        {
+            moreInstructionObject.SetActive(true);
+        }
 
-            if (vrCamera.text.Trim().Length == 0)
-            {
-                vrCameraObject.SetActive(true);
-            }
-
-
-
-            if (!(distraction.text.Trim().Length == 0))
-            {
-                distractionObject.SetActive(false);
-            }
-
-            if (!(distractionOnScreen.text.Trim().Length == 0))
-            {
-                distractionOnSObject.SetActive(false);
-            }
-
-            if (!(difficultyStudy.text.Trim().Length == 0))
-            {
-                difficultyStudyObject.SetActive(false);
-            }
-
-            if (!(answerWrong.text.Trim().Length == 0))
-            {
-                answerWrongObject.SetActive(false);
-            }
-
-            if (!(moreInstruction.text.Trim().Length == 0))
-            {
-                moreInstructionObject.SetActive(false);
-            }
-
-            if (!(vrCamera.text.Trim().Length == 0))
-            {
-                vrCameraObject.SetActive(false);
-            }
-            if (!(distraction.text.Trim().Length == 0) && !(distractionOnScreen.text.Trim().Length == 0) && !(difficultyStudy.text.Trim().Length == 0) && !(answerWrong.text.Trim().Length == 0) && !(moreInstruction.text.Trim().Length == 0) && !(vrCamera.text.Trim().Length == 0))
-            {
-                if (distractionObject.activeSelf == true)
-                {
-                    distractionObject.SetActive(false);
-                }
-                if (distractionOnSObject.activeSelf == true)
-                {
-                    distractionOnSObject.SetActive(false);
-                }
-                if (difficultyStudyObject.activeSelf == true)
-                {
-                    difficultyStudyObject.SetActive(false);
-                }
-                if (answerWrongObject.activeSelf == true)
-                {
-                    answerWrongObject.SetActive(false);
-                }
-                if (moreInstructionObject.activeSelf == true)
-                {
-                    moreInstructionObject.SetActive(false);
-                }
-                if (vrCameraObject.activeSelf == true)
-                {
-                    vrCameraObject.SetActive(false);
-                }
-                button.onClick.Invoke();
-
-            }
+        if (vrCamera.text.Trim().Length == 0)
+        {
+            vrCameraObject.SetActive(true);
         }
 
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+
+        if (!(distraction.text.Trim().Length == 0))
+        {
+            distractionObject.SetActive(false);
+        }
+
+        if (!(distractionOnScreen.text.Trim().Length == 0))
+        {
+            distractionOnSObject.SetActive(false);
+        }
+
+        if (!(difficultyStudy.text.Trim().Length == 0))
+        {
+            difficultyStudyObject.SetActive(false);
+        }
+
+        if (!(answerWrong.text.Trim().Length == 0))
+        {
+            answerWrongObject.SetActive(false);
+        }
+
+        if (!(moreInstruction.text.Trim().Length == 0))
+        {
+            moreInstructionObject.SetActive(false);
+        }
+
+        if (!(vrCamera.text.Trim().Length == 0))
+        {
+            vrCameraObject.SetActive(false);
+        }
+        if (!(distraction.text.Trim().Length == 0) && !(distractionOnScreen.text.Trim().Length == 0) && !(difficultyStudy.text.Trim().Length == 0) && !(answerWrong.text.Trim().Length == 0) && !(moreInstruction.text.Trim().Length == 0) && !(vrCamera.text.Trim().Length == 0))
+        {
+            if (distractionObject.activeSelf == true)
+            {
+                distractionObject.SetActive(false);
+            }
+            if (distractionOnSObject.activeSelf == true)
+            {
+                distractionOnSObject.SetActive(false);
+            }
+            if (difficultyStudyObject.activeSelf == true)
+            {
+                difficultyStudyObject.SetActive(false);
+            }
+            if (answerWrongObject.activeSelf == true)
+            {
+                answerWrongObject.SetActive(false);
+            }
+            if (moreInstructionObject.activeSelf == true)
+            {
+                moreInstructionObject.SetActive(false);
+            }
+            if (vrCameraObject.activeSelf == true)
+            {
+                vrCameraObject.SetActive(false);
+            }
+
+            //Hier kommen die zu nutzenden Objekte
+            lastText.SetActive(true);
+            postStudy.SetActive(false);
+            keyboard.SetActive(false);
+            canvasKeyboard.SetActive(false);
+
+
+        }
+    }
+
+    public void upperInputField()
+    {
+        if(buttonArrowUp.enabled == true)
         {
             InputSelected--;
             if (InputSelected < 0)
             {
                 InputSelected = 5;
             }
-            SelectInputField();
-        }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            InputSelected++;
-            if (InputSelected > 5)
-            {
-                InputSelected = 0;
-            }
-            SelectInputField();
-        }
-
-        void SelectInputField()
-        {
             switch (InputSelected)
             {
                 case 0:
@@ -189,6 +198,69 @@ public class ArrowInputFieldPostStudy : MonoBehaviour
                     vrCamera.Select();
                     break;
             }
+            buttonArrowUp.enabled = false;
         }
+        if(buttonBlockedUp > 1f)
+        {
+            buttonArrowUp.enabled = true;
+            buttonBlockedUp = 0;
+        }
+           
+        buttonArrowUp.interactable = false;
+        Invoke("ReActivateArrowUp", 1);
+    }
+
+    public void lowerInputField()
+    { 
+      if (buttonArrowDown.enabled == true)
+      {
+            InputSelected++;
+            if (InputSelected > 5)
+            {
+                InputSelected = 0;
+            }
+
+            switch (InputSelected)
+            {
+                case 0:
+                    difficultyStudy.Select();
+                    break;
+                case 1:
+                    answerWrong.Select();
+                    break;
+                case 2:
+                    moreInstruction.Select();
+                    break;
+                case 3:
+                    distraction.Select();
+                    break;
+                case 4:
+                    distractionOnScreen.Select();
+                    break;
+                case 5:
+                    vrCamera.Select();
+                    break;
+            }
+            buttonArrowDown.enabled = false;
+      }
+      if(buttonBlockedDown > 1f)
+        {
+            buttonArrowDown.enabled = true;
+            buttonBlockedDown  = 0;
+            
+        }
+      
+        buttonArrowDown.interactable = false;
+        Invoke("ReActivateArrowDown", 1);
+    }
+
+    public void ReActivateArrowDown()
+    {
+        buttonArrowDown.interactable = true;
+    }
+
+    public void ReActivateArrowUp()
+    {
+        buttonArrowUp.interactable = true;
     }
 }
