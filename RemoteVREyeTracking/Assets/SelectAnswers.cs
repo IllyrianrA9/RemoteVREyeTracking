d8f686bd949377bfa95593f8c17177cee1c76c19
Ante941 <72032365+Ante941@users.noreply.github.com>
2022-02-18 13:14:40 +0100

    ChoseTopic & Login pictures uploaded

--- RemoteVREyeTracking/Assets/SelectAnswers.cs
+++ RemoteVREyeTracking/Assets/SelectAnswers.cs
@@ -21,6 +21,13 @@ public class SelectAnswers : MonoBehaviour
     public GameObject topic6;
     public GameObject topic7;
     public GameObject topic8;
+    public GameObject topic9;
+    public GameObject topic10;
+    public GameObject topic11;
+    public GameObject topic12;
+    public GameObject topic13;
+    public GameObject topic14;
+    public GameObject topic15;
 
     public GameObject cancelVote1;
     public GameObject cancelVote2;
@@ -38,6 +45,13 @@ public class SelectAnswers : MonoBehaviour
     private Button topicButton6;
     private Button topicButton7;
     private Button topicButton8;
+    private Button topicButton9;
+    private Button topicButton10;
+    private Button topicButton11;
+    private Button topicButton12;
+    private Button topicButton13;
+    private Button topicButton14;
+    private Button topicButton15;
 
     private Button cancelVoteButton1;
     private Button cancelVoteButton2;
@@ -57,6 +71,13 @@ public class SelectAnswers : MonoBehaviour
         topicButton6 = topic6.GetComponent<Button>();
         topicButton7 = topic7.GetComponent<Button>();
         topicButton8 = topic8.GetComponent<Button>();
+        topicButton9 = topic9.GetComponent<Button>();
+        topicButton10 = topic10.GetComponent<Button>();
+        topicButton11 = topic11.GetComponent<Button>();
+        topicButton12 = topic12.GetComponent<Button>();
+        topicButton13 = topic13.GetComponent<Button>();
+        topicButton14 = topic14.GetComponent<Button>();
+        topicButton15 = topic15.GetComponent<Button>();
 
         cancelVoteButton1 = cancelVote1.GetComponent<Button>();
         cancelVoteButton2 = cancelVote2.GetComponent<Button>();
@@ -234,6 +255,146 @@ public class SelectAnswers : MonoBehaviour
         }
     }
 
+    public void selectTopic9()
+    {
+        if (string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer1.GetComponent<TMP_Text>().text = topicButton9.GetComponentInChildren<Text>().text;
+            topic9.SetActive(false);
+            cancelVote1.SetActive(true);
+        }
+        else if (string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer2.GetComponent<TMP_Text>().text = topicButton9.GetComponentInChildren<Text>().text;
+            topic9.SetActive(false);
+            cancelVote2.SetActive(true);
+        }
+        if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+        {
+            confirmSelection.SetActive(true);
+        }
+    }
+
+    public void selectTopic10()
+    {
+        if (string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer1.GetComponent<TMP_Text>().text = topicButton10.GetComponentInChildren<Text>().text;
+            topic10.SetActive(false);
+            cancelVote1.SetActive(true);
+        }
+        else if (string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer2.GetComponent<TMP_Text>().text = topicButton10.GetComponentInChildren<Text>().text;
+            topic10.SetActive(false);
+            cancelVote2.SetActive(true);
+        }
+        if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+        {
+            confirmSelection.SetActive(true);
+        }
+    }
+
+    public void selectTopic11()
+    {
+        if (string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer1.GetComponent<TMP_Text>().text = topicButton11.GetComponentInChildren<Text>().text;
+            topic11.SetActive(false);
+            cancelVote1.SetActive(true);
+        }
+        else if (string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer2.GetComponent<TMP_Text>().text = topicButton11.GetComponentInChildren<Text>().text;
+            topic11.SetActive(false);
+            cancelVote2.SetActive(true);
+        }
+        if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+        {
+            confirmSelection.SetActive(true);
+        }
+    }
+
+    public void selectTopic12()
+    {
+        if (string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer1.GetComponent<TMP_Text>().text = topicButton12.GetComponentInChildren<Text>().text;
+            topic12.SetActive(false);
+            cancelVote1.SetActive(true);
+        }
+        else if (string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer2.GetComponent<TMP_Text>().text = topicButton12.GetComponentInChildren<Text>().text;
+            topic12.SetActive(false);
+            cancelVote2.SetActive(true);
+        }
+        if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+        {
+            confirmSelection.SetActive(true);
+        }
+    }
+
+    public void selectTopic13()
+    {
+        if (string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer1.GetComponent<TMP_Text>().text = topicButton13.GetComponentInChildren<Text>().text;
+            topic13.SetActive(false);
+            cancelVote1.SetActive(true);
+        }
+        else if (string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer2.GetComponent<TMP_Text>().text = topicButton13.GetComponentInChildren<Text>().text;
+            topic13.SetActive(false);
+            cancelVote2.SetActive(true);
+        }
+        if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+        {
+            confirmSelection.SetActive(true);
+        }
+    }
+
+    public void selectTopic14()
+    {
+        if (string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer1.GetComponent<TMP_Text>().text = topicButton14.GetComponentInChildren<Text>().text;
+            topic14.SetActive(false);
+            cancelVote1.SetActive(true);
+        }
+        else if (string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer2.GetComponent<TMP_Text>().text = topicButton14.GetComponentInChildren<Text>().text;
+            topic14.SetActive(false);
+            cancelVote2.SetActive(true);
+        }
+        if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+        {
+            confirmSelection.SetActive(true);
+        }
+    }
+
+    public void selectTopic15()
+    {
+        if (string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer1.GetComponent<TMP_Text>().text = topicButton15.GetComponentInChildren<Text>().text;
+            topic15.SetActive(false);
+            cancelVote1.SetActive(true);
+        }
+        else if (string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text))
+        {
+            chosenAnswer2.GetComponent<TMP_Text>().text = topicButton15.GetComponentInChildren<Text>().text;
+            topic15.SetActive(false);
+            cancelVote2.SetActive(true);
+        }
+        if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+        {
+            confirmSelection.SetActive(true);
+        }
+    }
+
     public void cancelVote1WithClick()
     {
         if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton1.GetComponentInChildren<Text>().text))
@@ -256,7 +417,7 @@ public class SelectAnswers : MonoBehaviour
             cancelVote1.SetActive(false);
             topic2.SetActive(true);
         }
-        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton3.GetComponentInChildren<Text>().text))
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton3.GetComponentInChildren<Text>().text.Trim()))
         {
             if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
             {
@@ -316,6 +477,83 @@ public class SelectAnswers : MonoBehaviour
             cancelVote1.SetActive(false);
             topic8.SetActive(true);
         }
+
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton9.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer1.GetComponent<TMP_Text>().text = "";
+            cancelVote1.SetActive(false);
+            topic9.SetActive(true);
+        }
+
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton10.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer1.GetComponent<TMP_Text>().text = "";
+            cancelVote1.SetActive(false);
+            topic10.SetActive(true);
+        }
+
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton11.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer1.GetComponent<TMP_Text>().text = "";
+            cancelVote1.SetActive(false);
+            topic11.SetActive(true);
+        }
+
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton12.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer1.GetComponent<TMP_Text>().text = "";
+            cancelVote1.SetActive(false);
+            topic12.SetActive(true);
+        }
+
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton13.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer1.GetComponent<TMP_Text>().text = "";
+            cancelVote1.SetActive(false);
+            topic13.SetActive(true);
+        }
+
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton14.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer1.GetComponent<TMP_Text>().text = "";
+            cancelVote1.SetActive(false);
+            topic14.SetActive(true);
+        }
+
+        if (chosenAnswer1.GetComponent<TMP_Text>().text.Trim().Equals(topicButton15.GetComponentInChildren<Text>().text.Trim()))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer1.GetComponent<TMP_Text>().text = "";
+            cancelVote1.SetActive(false);
+            topic15.SetActive(true);
+        }
     }
 
     public void cancelVote2WithClick()
@@ -340,7 +578,7 @@ public class SelectAnswers : MonoBehaviour
             cancelVote2.SetActive(false);
             topic2.SetActive(true);
         }
-        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton3.GetComponentInChildren<Text>().text))
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton3.GetComponentInChildren<Text>().text.Trim()))
         {
             if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
             {
@@ -399,6 +637,84 @@ public class SelectAnswers : MonoBehaviour
             chosenAnswer2.GetComponent<TMP_Text>().text = "";
             cancelVote2.SetActive(false);
             topic8.SetActive(true);
+        }
+
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton9.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer2.GetComponent<TMP_Text>().text = "";
+            cancelVote2.SetActive(false);
+            topic9.SetActive(true);
+        }
+
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton10.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer2.GetComponent<TMP_Text>().text = "";
+            cancelVote2.SetActive(false);
+            topic10.SetActive(true);
+        }
+
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton11.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer2.GetComponent<TMP_Text>().text = "";
+            cancelVote2.SetActive(false);
+            topic11.SetActive(true);
+        }
+
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton12.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer2.GetComponent<TMP_Text>().text = "";
+            cancelVote2.SetActive(false);
+            topic12.SetActive(true);
+        }
+
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton13.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer2.GetComponent<TMP_Text>().text = "";
+            cancelVote2.SetActive(false);
+            topic13.SetActive(true);
+        }
+
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton14.GetComponentInChildren<Text>().text))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer2.GetComponent<TMP_Text>().text = "";
+            cancelVote2.SetActive(false);
+            topic14.SetActive(true);
+        }
+
+
+        if (chosenAnswer2.GetComponent<TMP_Text>().text.Trim().Equals(topicButton15.GetComponentInChildren<Text>().text.Trim()))
+        {
+            if (!(string.IsNullOrWhiteSpace(chosenAnswer1.GetComponent<TMP_Text>().text)) && !(string.IsNullOrWhiteSpace(chosenAnswer2.GetComponent<TMP_Text>().text)))
+            {
+                confirmSelection.SetActive(false);
+            }
+            chosenAnswer2.GetComponent<TMP_Text>().text = "";
+            cancelVote2.SetActive(false);
+            topic15.SetActive(true);
         }
     }
     public void confirmSelectionWithClick()
